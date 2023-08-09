68dafbc59076d508d5f010f6062b3de513926266
Ibrahim Essam <ibrahim.essam1995@gmail.com>
2023-08-09 15:13:09 +0100

    Add Dark Theme

--- ba_dca/gui.py
+++ ba_dca/gui.py
@@ -10,8 +10,10 @@ from PyQt5.QtWidgets import (
     QTableView,
     QVBoxLayout,
     QWidget,
-    QApplication
+    QApplication,
 )
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
 
 
 class Window(QMainWindow):
@@ -48,10 +50,42 @@ class Window(QMainWindow):
         self.layout.addWidget(self.table)
         self.layout.addLayout(layout)
 
+
+def enable_dark_theme(app: QApplication):
+    """Enable dark theme color palette.
+
+    Args:
+        app (QApplication): Qt application.
+    """
+    app.setStyle("Fusion")
+    # # Now use a palette to switch to dark colors:
+    dark_palette = QPalette()
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(35, 35, 35))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, QColor(35, 35, 35))
+    dark_palette.setColor(QPalette.Active, QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.darkGray)
+    dark_palette.setColor(QPalette.Disabled, QPalette.WindowText, Qt.darkGray)
+    dark_palette.setColor(QPalette.Disabled, QPalette.Text, Qt.darkGray)
+    dark_palette.setColor(QPalette.Disabled, QPalette.Light, QColor(53, 53, 53))
+    app.setPalette(dark_palette)
+
+
 def main():
     """Binance DCA main function."""
     # Create the application
     app = QApplication(sys.argv)
+    enable_dark_theme(app)
     # Create the main window
     win = Window()
     win.show()
